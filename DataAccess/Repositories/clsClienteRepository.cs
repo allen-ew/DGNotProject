cc741ed39c28c4e43f03d9efa53576086cf6d4ba
Allen <alejoyepez045@gmail.com>
2025-02-10 01:30:15 -0500

    Agregué las bases de DTO, Interfaces, Repositories

---
+++ DataAccess/Repositories/clsClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using DgNotification.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DgNotification.DataAccess.Repositories
+{
+    public  class clsClienteRepository : ICliente
+    {
+
+    }
+}
